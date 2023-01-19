54e503aa6cc39c7555ef814173d794428b8d5352
renanfreeiitas <38738883+renanfreeiitas@users.noreply.github.com>
2023-01-19 16:07:34 -0300

    Add files via upload

---
+++ multi.py
@@ -0,0 +1,17 @@
+import time
+import threading
+
+def fazer_requisicao_web():
+    print("fazendo requisicao web...")
+    time.sleep(3)
+    print("Terminei a requisicao")
+
+thread_1 = threading.Thread(target=fazer_requisicao_web)
+thread_1.start()
+
+thread_2 = threading.Thread(target=fazer_requisicao_web)
+thread_2.start()
+
+thread_3 = threading.Thread(target=fazer_requisicao_web)
+thread_3.start()
+
